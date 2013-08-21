e887fcb2dd30ca06b8807c6f712d9734f4ab2b48
Kamil Kułaga <teodozjan@gmail.com>
2013-08-21 11:17:23 +0200

    Create .emacs.el

---
+++ src/emacs/.emacs.el
@@ -0,0 +1,8 @@
+(defalias 'perl-mode 'cperl-mode)
+(column-number-mode t)
+(cua-mode 1)
+(show-paren-mode t)
+(desktop-save-mode 1)
+(global-hl-line-mode 1)
+(setq c-auto-newline 1)
+(setq cperl-auto-newline t)
