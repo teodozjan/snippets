471bac6f61663681f96debb145be4126ba0c0901
Kamil Kułaga <teodozjan@gmail.com>
2014-02-06 13:30:49 +0100

    Update stringopps.cpp

--- cpp/stringopps.cpp
+++ cpp/stringopps.cpp
@@ -3,3 +3,11 @@ const char* toConstChar(std::string strg)
 {
   return strg.c_str();
 }
+
+// Tracex howto
+void printTracex()
+{
+TRACEX_MESSAGE("NBSTR: %ls, stdString: %s",         
+         bstrSting,
+         strg.c_str();
+}
