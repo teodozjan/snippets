e20b9755c70a79eca0897060af78a82394b75931
Kamil Kułaga <teodozjan@gmail.com>
2014-02-25 15:05:32 +0100

    Update .emacs.el
    Don't want to line wrap logs

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -1,9 +1,11 @@
+(server-start)
 (defalias 'perl-mode 'cperl-mode)
 (column-number-mode t)
-;(cua-mode 1)
 (show-paren-mode t)
 ;(desktop-save-mode 1)
 (global-hl-line-mode 1)
 (setq c-auto-newline 1)
 (setq cperl-auto-newline t)
 (load-theme 'tango t)
+
+(add-hook 'text-mode-hook (lambda () (setq truncate-lines t)))
