468cb454cb0cf96ce51d22ac5639497fbf598ec4
Kamil Kułaga <teodozjan@gmail.com>
2014-05-23 10:00:10 +0200

    switched to emacs 24

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -1,12 +1,4 @@
 (server-start)
-(defalias 'perl-mode 'cperl-mode)
-(column-number-mode t)
 (show-paren-mode t)
-;(desktop-save-mode 1)
-(global-hl-line-mode 1)
-(setq c-auto-newline 1)
-(setq cperl-auto-newline t)
-(load-theme 'tango t)
-
+(load-theme 'whiteboard t)
 (add-hook 'text-mode-hook (lambda () (setq truncate-lines t)))
-(set-face-foreground 'font-lock-comment-face "slate gray")
