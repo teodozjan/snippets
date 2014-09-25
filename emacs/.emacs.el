4a553574f4a72cff111353538b148d2b2ad06bf7
Kamil Kułaga <teodozjan@users.noreply.github.com>
2014-09-25 17:14:01 +0200

    new

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -1,4 +1,11 @@
-(server-start)
+(column-number-mode t)
 (show-paren-mode t)
-(load-theme 'whiteboard t)
-(add-hook 'text-mode-hook (lambda () (setq truncate-lines t)))
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+(setq-default indent-tabs-mode nil)
+
+; begin POD
+(add-to-list 'load-path "C:/noinst/emacs-el")
+(require 'pod-mode)
+(add-to-list 'auto-mode-alist '("\\.pod$" . pod-mode))
+; end POD
