f862a209a9770f786e04a37f287063b028546356
Kamil Kułaga <teodozjan@users.noreply.github.com>
2014-09-25 17:15:38 +0200

    Update .emacs.el

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -1,5 +1,9 @@
+; View
 (column-number-mode t)
 (show-paren-mode t)
+
+
+; Lets disable some things that limits 
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 (setq-default indent-tabs-mode nil)
@@ -8,4 +12,4 @@
 (add-to-list 'load-path "C:/noinst/emacs-el")
 (require 'pod-mode)
 (add-to-list 'auto-mode-alist '("\\.pod$" . pod-mode))
-; end POD
+
