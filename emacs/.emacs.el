1f58c56a504eeb902ef40b2910494f66a62c8bd0
Kamil Kułaga <teodozjan@users.noreply.github.com>
2014-09-29 12:10:05 +0200

    Update .emacs.el

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -9,7 +9,12 @@
 (setq-default indent-tabs-mode nil)
 
 ; begin POD
-(add-to-list 'load-path "C:/noinst/emacs-el")
-(require 'pod-mode)
+(if (eq system-type 'windows-nt)
+  ; On unix like OS'es it's better to install 
+  ; emacs-goodies-el
+  (add-to-list 'load-path "C:/noinst/emacs-el")
+  (require 'pod-mode)
+)
+
 (add-to-list 'auto-mode-alist '("\\.pod$" . pod-mode))
 (add-to-list 'auto-mode-alist '("\\.t$" . perl-mode))
