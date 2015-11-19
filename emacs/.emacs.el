a6cbd05e6faebbbc336c4b21ec35167dc735d835
Kamil Kułaga <teodozjan@users.noreply.github.com>
2015-11-19 21:33:16 +0100

    Update .emacs.el

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -1,7 +1,7 @@
 ; View
 (column-number-mode t)
 (show-paren-mode t)
-
+(load-theme 'leuven t)
 
 ; Lets disable some things that limits 
 (put 'upcase-region 'disabled nil)
@@ -18,3 +18,8 @@
 
 (add-to-list 'auto-mode-alist '("\\.pod$" . pod-mode))
 (add-to-list 'auto-mode-alist '("\\.t$" . perl-mode))
+
+    (setq backup-directory-alist
+          `((".*" . ,temporary-file-directory)))
+    (setq auto-save-file-name-transforms
+          `((".*" ,temporary-file-directory t)))
