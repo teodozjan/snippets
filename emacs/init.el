16803160361e2ab2708dd0274a69c50f351022c9
Kamil Kułaga <teodozjan@users.noreply.github.com>
2021-05-19 12:08:17 +0200

    Update init.el

--- emacs/init.el
+++ emacs/init.el
@@ -29,7 +29,7 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 ;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-;(package-initialize)
+
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
