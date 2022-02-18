8a65d6baaa2f00340c331387647150809df4d8c2
Kamil Kułaga <teodozjan@users.noreply.github.com>
2022-02-18 16:33:53 +0100

    s/perl6/raku

--- emacs/init.el
+++ emacs/init.el
@@ -58,7 +58,7 @@
 (use-package cloud-theme :ensure :defer)
 (use-package nyx-theme :ensure :defer)
 
-(use-package perl6-mode
+(use-package raku-mode
   :ensure t
   :defer t)
 
@@ -109,7 +109,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(dired-sidebar vlf use-package persistent-scratch perl6-mode nyx-theme monokai-pro-theme logview hemera-theme circadian)))
+   '(dired-sidebar vlf use-package persistent-scratch raku-mode nyx-theme monokai-pro-theme logview hemera-theme circadian)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
